a364c169269995f5f68fce72b2c77e1d1ff96bd3
Ayush Kumar <115335623+Ayush-28-08@users.noreply.github.com>
2023-01-03 12:44:02 +0530

    final done

--- custom-hooks/src/App.js
+++ custom-hooks/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import useStorage from './components/useStorage';
 
 function App() {
+  const [content, setContent] = useStorage('content','')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="text" value={content} onChange={e=>setContent(e.target.value)}></input>
     </div>
   );
 }
